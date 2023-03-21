9bd4a47f043926479a8fa8c3313fb4d0bf5526a7
SalteaRS <102844491+SalteaRS@users.noreply.github.com>
2023-03-21 22:50:08 +0000

    added comments to the actual code

--- FairyNuff/src/main/java/commands/embedCommands/EmbedCommandExample.java
+++ FairyNuff/src/main/java/commands/embedCommands/EmbedCommandExample.java
@@ -6,15 +6,16 @@ public class EmbedCommandExample extends EmbedCommandTemplate {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        title = "Hi, this is a test";
-        description = "Haha description go brrrrrr";
-        color = 0xFD9D06;
+        title = "Hi, this is a test"; //it says title but it's really just the first header
+        description = "Haha description go brrrrrr"; //it says description but it's really just the first bit of text. This is usually left blank. when it's blank it will be -> description = "";
+        color = 0xFD9D06; //this is the colour on the left hand side of the embed
 
-        addField("Title of field 1", "Description of field 1");
-        addField("Another field! :o", "Wow it's like magic!\nAlso, because of that backslash + n, this is on a new line!");
+        addField("Title of field 1", "Description of field 1");                                                                                                //the first "text here", is the title whilst the second is a description
+        addField("Another field! :o", "Wow it's like magic!\nAlso, because of that backslash + n, this is on a new line!");                                    //if you want to add a break (enter key) you would use \n note that if you add a space between the \n and your text there will be a space before the text
         addField("I don't believe fields can have emojis <:KEKWait:673504466871386112>", "But descriptions should be able to <:KEKW:669922267676934164>");
         addField("Links can be done in this format", "[Text goes here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)");
 
-        super.execute(event);
+        super.execute(event); //this HAS to be there
     }
 }
+// this is what it comes out as https://media.discordapp.net/attachments/862423583400394834/1087859419666202716/image.png

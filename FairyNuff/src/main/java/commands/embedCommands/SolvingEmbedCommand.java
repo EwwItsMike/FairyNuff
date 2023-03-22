79993b964a64b1bc2e7d1a2f3811cd688132eaa2
mlandHanze <120571801+EwwItsMike@users.noreply.github.com>
2023-03-22 15:36:45 +0100

    Solved some issues with FaqEmbedCommand and SolvingEmbedCommand
    String parameters were too long in FaqEmbedCommand. Split them up into different fields with empty titles, but it doesn't look good yet.
    Fixed the crediting of Preset Maker in SolvingEmbedCommand, linking to users does not work in field titles.

--- FairyNuff/src/main/java/commands/embedCommands/SolvingEmbedCommand.java
+++ FairyNuff/src/main/java/commands/embedCommands/SolvingEmbedCommand.java
@@ -10,7 +10,7 @@ public class SolvingEmbedCommand extends EmbedCommandTemplate {
         description = "";
         color = 0xEC1568;
 
-        addField("__Solving Preset Maker__ (Courtesy of <@145884917627224065>, <@300296497277173761>", "The [Preset Maker](https://discord.com/channels/332595657363685377/1084955288437784666) will make you optimal presets based on what you have\n*Note:it is still in beta and the creators are asking for suggestions and feedback.*");
+        addField("__Solving Preset Maker__", "Courtesy of <@145884917627224065>, <@300296497277173761>\nThe [Preset Maker](https://discord.com/channels/332595657363685377/1084955288437784666) will make you optimal presets based on what you have\n*Note: it is still in beta and the creators are asking for suggestions and feedback.*");
         addField("__Easy Clues__ <:Easy:633728862039179294>", "[Preset](https://discord.com/channels/332595657363685377/1023171302405718026) - [Step-by-step guide](https://clue-guide.web.app/clues)");
         addField("__Medium Clues__ <:Medium:633728704438075435>", "[Preset](https://discord.com/channels/332595657363685377/1023171953235873895) - [Step-by-step guide](https://docs.google.com/spreadsheets/d/1DYlbYYRRx_EvEXbpzVJI15GxWQeF8hQj424pzqi1Sr0/edit#gid=1510876691)");
         addField("__Hard Clues__ <:Hard:633728361918758930>", "[Preset](https://discord.com/channels/332595657363685377/1071930109000429630) - [Step-by-step guide](https://docs.google.com/spreadsheets/d/1KLE216RvVQOhOz1ItTY7KPSxZQPN0ybXuQvmUDf3J2M/edit#gid=147294503) - [Requirements](https://media.discordapp.net/attachments/1023179363660861531/1023179364042559549/unknown.png)");
